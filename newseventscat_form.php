17dac3cedcc452052d5f649c104227b982057fba
Ganesh Kumar <ganesh@capecom.co.uk>
2025-03-03 13:55:49 +0530

    Update newseventscat_form.php

--- newseventscat_form.php
+++ newseventscat_form.php
@@ -233,7 +233,7 @@ $parent_category_list = $db->get_rsltset($parent_category);
                     <div class="col-md-9">
                       <div class="pad-tb-7">
                         <input type="checkbox" data-plugin="switchery" value=<?php echo ($res_ed['isactive'] == 0) ? '0' : '1'; ?> 
-                         name="chkstatus" id="chkstatus" <?php echo $chk; ?> data-color="#00b19d" data-size="small" checked />
+                         name="chkstatus" id="chkstatus" data-color="#00b19d" data-size="small" <?php echo $chk; ?>/>
                       </div>
                     </div>
                   </div>
